f4fff00a113ba33cec617b76a665973404070e7b
Alain Schaerer <alainsch@buffalo.edu>
2024-05-06 10:49:15 -0400

    Added andgate

---
+++ andgate/and.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:08:40 04/16/2024 
+-- Design Name: 
+-- Module Name:    and - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity and is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+           Y : out  STD_LOGIC);
+end and;
+
+architecture Behavioral of and is
+
+begin
+
+
+end Behavioral;
+
